7bf6d7b27fdbefab98e3a98ca3e4c38b2c166f9c
Claudia <caucrah@gmail.com>
2023-06-03 12:09:25 +0200

    Add Sidebar

--- twitter-project/components/Layout.tsx
+++ twitter-project/components/Layout.tsx
@@ -1,20 +1,26 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Grid, Container, GridItem } from "@chakra-ui/react";
+import { Sidebar } from "./Sidebar";
 
 interface LayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps) => {
-    return (
-        <Flex
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="flex-start"
-            width="100%"
-            maxWidth="1200px"
-            margin="0 auto"
-        >
+  return (
+    <Box bg="black" h="100vH">
+      <Container h="full" maxW="6xl" px={{ base: "0", xl: "30" }}>
+        <Grid templateColumns={{ base: "repeat(4, 1fr)" }} h="full">
+          <Sidebar />
+          <GridItem
+            colSpan={{ base: 3, lg: 2 }}
+            borderX="1px"
+            borderColor="gray.700"
+            minH="full"
+          >
             {children}
-        </Flex>
-    );
-}
+          </GridItem>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
---
+++ twitter-project/components/Sidebar.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+
+interface SidebarProps {}
+
+export const Sidebar = () => {
+  return <Box></Box>;
+};
