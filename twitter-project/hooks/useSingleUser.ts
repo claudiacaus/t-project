a2a31149c950aaf1d2f4e0eff3192cdb3b3c8f5a
Claudia <caucrah@gmail.com>
2023-06-05 17:35:42 +0200

    Add useUsers and useSingleUser hooks

---
+++ twitter-project/hooks/useSingleUser.ts
@@ -0,0 +1,18 @@
+import useSWR from "swr";
+import fetcher from "@/libs/fetcher";
+
+const useSingleUser = (userId: string) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    userId ? `/api/users/${userId}` : null,
+    fetcher
+  );
+
+  return {
+    data,
+    error,
+    isLoading,
+    mutate,
+  };
+};
+
+export default useSingleUser;
