cd23e27333719a1663080b1147b64d889948fd4f
Cláudia (de Windt) Pereira Oliveira <55986532+claudiadewindt@users.noreply.github.com>
2023-06-07 11:19:44 +0200

    Delete build directory

--- twitter-project/theme/components/container.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentStyleConfig } from '@chakra-ui/react';
-
-export const container: ComponentStyleConfig = {
-  baseStyle: {
-    maxWidth: '100%',
-    width: 'container.xl',
-    px: '16px',
-  },
-};
